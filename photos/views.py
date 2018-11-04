4a838f78c150aa68f8bc091a416d61fe28399133
MaryannGitonga <naygitonga4@gmail.com>
2018-11-04 18:55:12 +0300

    Add index view function

---
+++ photos/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,redirect
+from .models import Image,Category,Location
+from django.http import Http404
+
+# Create your views here.
+def index(request):
+    images = Image.objects.all()
+    locations = Location.objects.all()
+    return render(request,'index.html',{'images':images,'locations':locations})
