f711cd041b1464ea2f632aa2cdb91ff41aaad254
MaryannGitonga <naygitonga4@gmail.com>
2018-11-04 18:53:56 +0300

    Add index url

---
+++ photos/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+from django.views.generic import RedirectView
+
+urlpatterns=[
+    url(r'^$',views.index,name='index'),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
