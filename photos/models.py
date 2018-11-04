2ad733d04d1c71daec3ca1066266267f1deab3b2
MaryannGitonga <naygitonga4@gmail.com>
2018-11-04 18:49:03 +0300

    Add location model class

--- photos/models.py
+++ photos/models.py
@@ -13,3 +13,16 @@ class Category(models.Model):
     @classmethod
     def delete_category(cls,name):
         cls.objects.filter(name = name).delete()
+
+class Location(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    @classmethod
+    def delete_location(cls,name):
+        cls.objects.filter(name = name).delete()
