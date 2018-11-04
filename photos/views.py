6d0096647438cfd9b26cc7380bac1fb5b4319fed
MaryannGitonga <naygitonga4@gmail.com>
2018-11-04 18:55:33 +0300

    Add display location view function

--- photos/views.py
+++ photos/views.py
@@ -7,3 +7,12 @@ def index(request):
     images = Image.objects.all()
     locations = Location.objects.all()
     return render(request,'index.html',{'images':images,'locations':locations})
+
+def display_location(request,location_id):
+    try:
+        locations = Location.objects.all()
+        location = Location.objects.get(id = location_id)
+        images = Image.objects.filter(image_location = location.id)
+    except:
+        raise Http404()
+    return render(request,'location.html',{'location':location,'images':images,'locations':locations})
