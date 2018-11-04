19a3786761763003b10429c1ce47f565f5663e45
MaryannGitonga <naygitonga4@gmail.com>
2018-11-04 18:49:18 +0300

    Add image model class

--- photos/models.py
+++ photos/models.py
@@ -26,3 +26,18 @@ class Location(models.Model):
     @classmethod
     def delete_location(cls,name):
         cls.objects.filter(name = name).delete()
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    image_name = models.CharField(max_length = 30)
+    image_description = models.TextField()
+    image_category = models.ForeignKey(Category,on_delete = models.CASCADE)
+    image_location = models.ForeignKey(Location,on_delete = models.CASCADE)
+
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def search_by_category(cls,search_term):
+        images = cls.objects.filter(image_category__name__contains = search_term)
+        return images
